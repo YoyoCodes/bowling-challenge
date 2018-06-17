4cb353533ee588afabb43a80e5b8115942c5b084
Laura Petre <laura.d.petre@gmail.com>
2018-06-17 15:42:57 +0100

    2nd test - before refractoring

--- jasmine-standalone-3.1.0/spec/BowlingGameSpec.js
+++ jasmine-standalone-3.1.0/spec/BowlingGameSpec.js
@@ -1,11 +1,19 @@
 describe("BowlingGame", function() {
   var bowlingGame;
 
-  it ('can roll a Gutter Game', function(){
+  it ('can score a Gutter Game', function(){
     bowlingGame = new BowlingGame();
     for(var i=0; i<21; i++){
       bowlingGame.roll(0);
     }
     expect(bowlingGame.score()).toEqual(0)
+  });
+
+  it('can score a game of incomplete frames', function(){
+    bowlingGame = new BowlingGame();
+    for(var i=0; i<20; i++){
+      bowlingGame.roll(2);
+    }
+    expect(bowlingGame.score()).toEqual(40)
   });
 });
--- jasmine-standalone-3.1.0/src/BowlingGame.js
+++ jasmine-standalone-3.1.0/src/BowlingGame.js
@@ -1,10 +1,11 @@
 function BowlingGame() {
+  this.current_score = 0;
 }
 
-BowlingGame.prototype.roll = function() {
-
+BowlingGame.prototype.roll = function(pins) {
+  this.current_score += pins
 };
 
 BowlingGame.prototype.score = function() {
-  return 0;
+  return this.current_score
 };
